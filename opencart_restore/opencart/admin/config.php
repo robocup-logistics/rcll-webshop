71a115310413361525f28e7cf8f09c18c7303d1a
Niklas Sebastian Eltester <eltester@fh-aachen.de>
2021-01-07 18:35:34 +0100

    Change web root location from html to http

--- opencart_restore/opencart/admin/config.php
+++ opencart_restore/opencart/admin/config.php
@@ -8,18 +8,18 @@ define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
 define('HTTPS_CATALOG', 'http://localhost/opencart/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/opencart/admin/');
-define('DIR_SYSTEM', '/var/www/html/opencart/system/');
-define('DIR_IMAGE', '/var/www/html/opencart/image/');
-define('DIR_LANGUAGE', '/var/www/html/opencart/admin/language/');
-define('DIR_TEMPLATE', '/var/www/html/opencart/admin/view/template/');
-define('DIR_CONFIG', '/var/www/html/opencart/system/config/');
-define('DIR_CACHE', '/var/www/html/opencart/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/opencart/system/storage/download/');
-define('DIR_LOGS', '/var/www/html/opencart/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/html/opencart/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/html/opencart/system/storage/upload/');
-define('DIR_CATALOG', '/var/www/html/opencart/catalog/');
+define('DIR_APPLICATION', '/var/www/http/opencart/admin/');
+define('DIR_SYSTEM', '/var/www/http/opencart/system/');
+define('DIR_IMAGE', '/var/www/http/opencart/image/');
+define('DIR_LANGUAGE', '/var/www/http/opencart/admin/language/');
+define('DIR_TEMPLATE', '/var/www/http/opencart/admin/view/template/');
+define('DIR_CONFIG', '/var/www/http/opencart/system/config/');
+define('DIR_CACHE', '/var/www/http/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/http/opencart/system/storage/download/');
+define('DIR_LOGS', '/var/www/http/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/http/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/http/opencart/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/http/opencart/catalog/');
 
 // DB
 define('DB_DRIVER', 'mpdo');
--- opencart_restore/opencart/config.php
+++ opencart_restore/opencart/config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://localhost/opencart/');
 define('HTTPS_SERVER', 'http://localhost/opencart/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/opencart/catalog/');
-define('DIR_SYSTEM', '/var/www/html/opencart/system/');
-define('DIR_IMAGE', '/var/www/html/opencart/image/');
-define('DIR_LANGUAGE', '/var/www/html/opencart/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/html/opencart/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/html/opencart/system/config/');
-define('DIR_CACHE', '/var/www/html/opencart/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/opencart/system/storage/download/');
-define('DIR_LOGS', '/var/www/html/opencart/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/html/opencart/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/html/opencart/system/storage/upload/');
+define('DIR_APPLICATION', '/var/www/http/opencart/catalog/');
+define('DIR_SYSTEM', '/var/www/http/opencart/system/');
+define('DIR_IMAGE', '/var/www/http/opencart/image/');
+define('DIR_LANGUAGE', '/var/www/http/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/http/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/http/opencart/system/config/');
+define('DIR_CACHE', '/var/www/http/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/http/opencart/system/storage/download/');
+define('DIR_LOGS', '/var/www/http/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/http/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/http/opencart/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mpdo');
